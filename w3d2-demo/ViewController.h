e623c2ca88cb8ed95b9b715b17bea832b25d4692
Roland Tecson <roland_tecson@hotmail.com>
2018-08-14 09:28:38 -0700

    Initial Commit

---
+++ w3d2-demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  w3d2-demo
+//
+//  Created by Roland on 2018-08-14.
+//  Copyright © 2018 Lighthouse Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ w3d2-demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  w3d2-demo
+//
+//  Created by Roland on 2018-08-14.
+//  Copyright © 2018 Lighthouse Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
